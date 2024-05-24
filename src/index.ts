963a156a3a10ad9e2ecb291f0fda200c0ec84f80
Qui Nguyen <quinguyen@Quis-MacBook-Pro.local>
2024-05-24 15:50:47 +0700

    feat: add stripe webhook

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,13 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 import cors from "cors";
 import { stripe } from "./lib/stripe.js";
 import { connectDB } from "./db/connect.js";
+import Stripe from "stripe";
+import BillingAddress from "./models/billingAddress.model.js";
+import ShippingAddress from "./models/shippingAddress.model.js";
+import Order from "./models/order.model.js";
 // import kindeNode from "@kinde-oss/kinde-node";
 
-const endpointSecret =
-  "whsec_4ea0b36c3d08d3cf10406422a70450b5869eab9d89156403b583803418d9f7e0";
+const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET_KEY!;
 
 dotenv.config();
 
@@ -34,48 +37,107 @@ app.get("/", (req, res) => {
 });
 
 app.post(
-  "/webhook",
+  "/api/webhooks",
   express.raw({ type: "application/json" }),
-  (request, response) => {
-    const sig = request.headers["stripe-signature"];
-
-    let event;
-
+  async (request, response) => {
     try {
-      event = stripe.webhooks.constructEvent(
-        request.body,
-        sig!,
-        endpointSecret
-      );
-    } catch (err) {
-      response.status(400).send(`Webhook Error: ${err.message}`);
-      return;
-    }
+      const sig = request.headers["stripe-signature"];
 
-    console.log("🚀 ~ event:", event);
-    // Handle the event
-    switch (event.type) {
-      case "payment_intent.succeeded":
-        const paymentIntentSucceeded = event.data.object;
-        console.log("🚀 ~ paymentIntentSucceeded:", paymentIntentSucceeded);
-        // Then define and call a function to handle the event payment_intent.succeeded
-        break;
-      // ... handle other event types
-      default:
-        console.log(`Unhandled event type ${event.type}`);
-    }
+      let event: null | Stripe.Event;
 
-    // Return a 200 response to acknowledge receipt of the event
-    response.send();
+      try {
+        event = stripe.webhooks.constructEvent(
+          request.body,
+          sig!,
+          endpointSecret
+        );
+      } catch (err) {
+        response.status(400).send(`Webhook Error: ${err.message}`);
+        return;
+      }
+
+      // Handle the event
+      if (!event) {
+        throw new Error("Event is not defined");
+      }
+
+      switch (event.type) {
+        case "checkout.session.completed":
+          if (!event.data.object.customer_details?.email) {
+            throw new Error("Email is not defined");
+          }
+
+          const session = event.data.object as Stripe.Checkout.Session;
+
+          console.log("🚀 ~ session:", session);
+          const { userId, orderId } = session.metadata || {
+            userId: null,
+            orderId: null,
+          };
+
+          if (!userId || !orderId) {
+            throw new Error("User or Order Id is not defined");
+          }
+
+          const billingAddress = session.customer_details!.address;
+          const shippingAddress = session.shipping_details!.address;
+
+          const newBillingAddress = new BillingAddress({
+            name: session.customer_details!.name,
+            city: billingAddress!.city,
+            country: billingAddress!.country,
+            street: billingAddress!.line1,
+            postalCode: billingAddress!.postal_code,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            state: billingAddress!.state,
+          });
+
+          const newBillingData = await newBillingAddress.save();
+
+          const newShippingAddress = new ShippingAddress({
+            name: session.customer_details!.name,
+            city: shippingAddress!.city,
+            country: shippingAddress!.country,
+            street: shippingAddress!.line1,
+            postalCode: shippingAddress!.postal_code,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            state: shippingAddress!.state,
+          });
+
+          const newShippingData = await newShippingAddress.save();
+
+          await Order.findOneAndUpdate(
+            {
+              _id: orderId,
+            },
+            {
+              isPaid: true,
+              billingAddressId: newBillingData._id,
+              shippingAddressId: newShippingData._id,
+              updatedAt: new Date(),
+            }
+          );
+
+          break;
+        default:
+          console.log(`Unhandled event type ${event.type}`);
+      }
+
+      // Return a 200 response to acknowledge receipt of the event
+      response.send({
+        event,
+        ok: true,
+      });
+    } catch (error) {
+      response.send({
+        message: "Something went wrong",
+        ok: false,
+      });
+    }
   }
 );
-
-// let authorize;
-
-// (async () => {
-//   authorize = await kindeNode("https://casemoishop.kinde.com");
-//   console.log("🚀 ~ authorize:", authorize);
-// })();
 
 (async () => {
   try {
@@ -90,20 +152,6 @@ app.post(
       typeDefs: mergeTypeDefs,
       resolvers,
       context: async ({ req }) => {
-        // const kindeUser = new Promise((resolve, reject) => {
-        //   authorize(req, (err, kindeUser) => {
-        //     if (err) {
-        //       return reject(err);
-        //     }
-        //     resolve(kindeUser);
-        //   });
-        // });
-
-        // console.log("🚀 ~ kindeUser ~ kindeUser:", kindeUser);
-
-        // const tokenHeader = req.headers.authorization || "";
-        // const token = tokenHeader.replace("Bearer ", "");
-        // const user = await getUserFromToken(token, db);
         const kindeUserId = req.headers.kinde_user_id;
         return { db, kindeUserId };
       },
